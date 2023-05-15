2315f20b2cfb4d8f1598c1259e19d06e679b9bb5
Josh <jmast1161@gmail.com>
2023-05-14 23:14:53 -0400

    Add menu scene and transition to game scene

---
+++ Assets/Scripts/MenuGameManager.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuGameManager : MonoBehaviour
+{
+    private Canvas mainMenu;
+    [SerializeField] private Canvas mainMenuPrefab;
+
+    void Start()
+    {
+        mainMenu = Instantiate(mainMenuPrefab, new Vector3(0f, 0f), Quaternion.identity);
+        var buttons = mainMenu.GetComponentsInChildren<UnityEngine.UI.Button>();
+        var entryAnimator = mainMenu.GetComponentInChildren<Animator>();
+        if(entryAnimator != null)
+        {
+            entryAnimator.Play("LevelCompletePanel", 0);
+        }
+
+        var playButton = buttons.FirstOrDefault(b => b.name == "Play");
+
+        if(playButton != null)
+        {
+            playButton.onClick.AddListener(() => 
+            {
+                SceneManager.LoadScene("GameScene");
+            });
+        }
+
+        var quitButton = buttons.FirstOrDefault(b => b.name == "Quit");
+
+        if(quitButton != null)
+        {
+            quitButton.onClick.AddListener(() => 
+            {
+                Application.Quit();
+            });
+        }
+    }
+}
